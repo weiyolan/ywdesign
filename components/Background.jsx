976bf47ab33686487e1cbc06d82e82c8f3fde306
weiyolan <yolan.weiler@gmail.com>
2023-01-16 20:56:55 +0100

    accessibility

--- components/Background.jsx
+++ components/Background.jsx
@@ -31,39 +31,40 @@ export default function Background ({scrolled}) {
   return (
     <div className={`-z-[50] w-full fixed top-0 h-screen overflow-hidden filter bg-cover bg-[url('/images/background.svg')] sm:bg-auto sm:bg-blue`} >
 
+      {width>breakPointSmall && 
         <div className={`bg-green ${myWidth<640?'':'animate-blob'} animation-delay-2000   
           h-[200vh] w-[66vw] 
           -left-[40vw] -bottom-[100vh] 
           transition-all duration-400 rounded-full filter absolute ${blurOn?`blur-[200px]`:''} `}
-          /> 
-        <div className={`bg-blue ${myWidth<640?'':'animate-blob2'} animation-delay-4000   
+          /> }
+          {width>breakPointSmall && <div className={`bg-blue ${myWidth<640?'':'animate-blob2'} animation-delay-4000   
           h-[200vh] w-[66vw] 
           right-[25vw] -top-[33vh] 
           transition-all duration-400 rounded-full filter absolute ${blurOn?`blur-[200px]`:''} `}
-          />
-        <div style={myWidth<640?{top:0,left:0}:{top:`${-60+25*scrolled}vh`, left:`${-33+30*scrolled}vw`}} 
+          />}
+           {width>breakPointSmall &&<div style={myWidth<640?{top:0,left:0}:{top:`${-60+25*scrolled}vh`, left:`${-33+30*scrolled}vw`}} 
         className={`bg-green ${myWidth<640?'':'animate-blob'} animation-delay-6000   
           h-[100vh] w-[66vw] 
           -left-[33vw] -top-[60vh] 
           transition-all ease-in-out duration-1000 rounded-full filter absolute ${blurOn?`blur-[180px] sm:blur-[200px]`:''} `}
-          /> 
-        <div className={`bg-green ${myWidth<640?'':'animate-blob2'} animation-delay-2000  
+          /> }
+          {width>breakPointSmall && <div className={`bg-green ${myWidth<640?'':'animate-blob2'} animation-delay-2000  
           h-[200vh] w-[66vw] 
           right-[0] -bottom-[60vh]
           transition-all duration-400 rounded-full filter absolute ${blurOn?`blur-[200px]`:''} `}
-          /> 
+          /> }
 
-        <div style={myWidth<640?{}:{bottom:`${-100+20*scrolled}vh`, right:`${-25+50*scrolled}vw`}}
+          {width>breakPointSmall &&<div style={myWidth<640?{}:{bottom:`${-100+20*scrolled}vh`, right:`${-25+50*scrolled}vw`}}
         className={`bg-blue ${myWidth<640?'':'animate-blob'} animation-delay-4000    
          -right-[25vw]  -bottom-[100vh]
          h-[150vh] w-[50vw]
           transition-all ease-in-out duration-1000 rounded-full filter absolute ${blurOn?`blur-[200px]`:''} `}
-          />
-        <div className={`bg-green ${myWidth<640?'top-0 right-0':'animate-blob'} animation-delay-0000   
+          />}
+           {width>breakPointSmall &&<div className={`bg-green ${myWidth<640?'top-0 right-0':'animate-blob'} animation-delay-0000   
           h-[200vh] w-[66vw] 
           -right-[33vw] -top-[150vh]
           transition-all duration-400 rounded-full filter absolute ${blurOn?`blur-[150px] sm:blur-[200px]`:''} `}
-          /> 
+          /> }
 
     </div>
   )
