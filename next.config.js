d090ce328410a6146e5904c5ac2bf1d08dd6c908
weiyolan <yolan.weiler@gmail.com>
2023-01-26 18:38:49 +0100

    french language

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable:process.env.NODE_ENV === 'development'
+  },
   images: {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
@@ -22,6 +31,10 @@ const nextConfig = {
   },
 
   reactStrictMode: true,
+});
+
+const nextConfig = {
+  
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
--- pages/_document.js
+++ pages/_document.js
@@ -9,10 +9,10 @@ export default function Document() {
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="manifest" href="/manifest.json"/>
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#6600ff"/>
         <meta name="msapplication-TileColor" content="#00aba9"/>
-        <meta name="theme-color" content="#171b4d"/>
+        <meta name="theme-color" content="#165174"/>
 
         
       </Head>
